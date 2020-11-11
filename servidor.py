7cdcd22aab6fec2c13e5a05798bacea7402eaaf8
codebreaker <norepply@codebreaker.com.br>
2020-11-10 21:05:10 -0300

    Criação do Servidor e cliente

--- servidor.py
+++ servidor.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 
 # importando bibliotecas
-
 import socket
-from _thread import *
-import sys
-import select
 
 # Variaveis
-#hostname = socket.gethostname() # endereço público
-hostname = "localhost" # endereço local
-porta = 1345 # porta de comunicação
+hostname = "localhost"                                                          # endereço local
+porta = 1404                                                                    # porta de comunicação
+tamanhoBuffer = 4*(1024)                                                        # Tamanho do Buffer
 
-chatServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-chatServidor.bind((hostname, porta))
-chatServidor.listen(2)
+# Criando Sockets TCP em IPv6
+chatServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                # Cria Socket TCP
+chatServidor.bind((hostname, porta))                                            # hostname e porta de acesso
+chatServidor.listen(30)                                                         # numero de Conexões permitidas
 
+# Função para calcular o fatorial de um numero
 def fatorial (numeroInteiro):
     tamanho = numeroInteiro + 1
     calculo = 1
@@ -23,22 +21,17 @@ def fatorial (numeroInteiro):
         calculo = calculo * i
     return calculo
 
-while True:
+# Loop infinito, aguardando requisições
+try:                                                                        # tratamento de erros
+    while True:
+        clienteSocket, enderecoCliente = chatServidor.accept()              # Espera por conexão do cliente
+        mensagem = clienteSocket.recv(tamanhoBuffer)                        # recebe a mensagem do Cliente
+        mensagem = mensagem.decode("ascii")                                 # converte os bytes na tabela ascii
+        valor = int(mensagem)                                               # converte a string em numero inteiro
+        fat = fatorial(valor)                                               # realiza os calculos
+        mensagem = str(fat).encode("utf-8")                                 # converte o numero inteiro em string
+        clienteSocket.send(mensagem)                                        # retorna a mensagem processada ao cliente
 
-    clienteSocket, enderecoCliente = chatServidor.accept()
-
-    mensagem = chatServidor.recv(1024)
-
-    mensagem = mensagem.decode("ascii")
-    #valor = int(mensagem)
-
-    #fat = fatorial(valor)
-
-    print(mensagem)
-
-    #mensagem = str(fat).encode("utf-8")
-
-    #clienteSocket.send(mensagem)
-
-
+except KeyboardInterrupt:                                                   # caso haja erro irá retornar uma ação
+    chatCliente.close()                                                     # finaliza conexão, finaliza o servidor
 
